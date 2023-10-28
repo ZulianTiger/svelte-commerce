74a06a8292e502214713580418a780c52eccc4f2
ZulianTiger <ady0606@gmail.com>
2023-10-28 22:08:48 +0200

    Lesson 4.7: Creating the category model

---
+++ prisma/migrations/20231028195618_add_category_model/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - Added the required column `category_id` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "category_id" INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_id_key" ON "Category"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_slug_key" ON "Category"("slug");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

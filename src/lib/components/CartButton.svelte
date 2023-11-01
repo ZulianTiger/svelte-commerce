73319e801d278da774a1cb8543472ce105e3ee89
ZulianTiger <ady0606@gmail.com>
2023-11-02 00:42:45 +0100

    Lesson 6.9: Creating a Layout

--- src/lib/components/CartButton.svelte
+++ src/lib/components/CartButton.svelte
@@ -1,3 +1,3 @@
-<div class="border-white rounded-lg p-2 hover:border-blue-500 duration-300">
-	<img src="shopping-cart.png" alt="Cart" />
+<div class="border border-white rounded-lg p-2 hover:border-blue-500 duration-300">
+	<img src="shopping-cart.png" alt="Cart" class="h-4" />
 </div>

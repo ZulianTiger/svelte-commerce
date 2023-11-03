912081570e75e82325e616036862b8b50f4118fe
ZulianTiger <ady0606@gmail.com>
2023-11-03 13:08:28 +0100

    Lesson 7.4: Looping the products

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,9 @@
 
 <p class="text-white">
 	{#if data.products.length > 0}
-		{JSON.stringify(data)}
+		{#each data.products as product}
+			<p>{product.name} / {product.price} / {product.description}</p>
+		{/each}
 	{:else}
 		<p class="w-full text-center pt-32">No products are currently available</p>
 	{/if}

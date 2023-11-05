60a06850b8da6b59b7ac18ea4e528996af4edec7
ZulianTiger <ady0606@gmail.com>
2023-11-05 16:56:06 +0100

    Lesson 8.2: Creating the CategoryItem component

---
+++ src/lib/components/CategoryItem.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let name;
+	export let slug;
+	export let active;
+
+	import { classes } from '$lib/helpers/classes';
+</script>
+
+<li>
+	<a
+		href={`/categories${slug}`}
+		class={classes(
+			'hover:text-blue-500 duration-300',
+			active ? 'text-blue-500 underline' : 'text-white'
+		)}
+	>
+		{name}
+	</a>
+</li>

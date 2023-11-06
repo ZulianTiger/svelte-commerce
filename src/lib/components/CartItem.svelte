93e69e88589bc86670c564e523afe9ce018faec6
ZulianTiger <ady0606@gmail.com>
2023-11-06 21:06:41 +0100

    Lesson 10.3: Creating the CartItem component

---
+++ src/lib/components/CartItem.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let index;
+	export let product;
+</script>
+
+<div class="group relative grid grid-cols-6 gap-2 border-b border-neutral-600">
+	<div>
+		<img src={product.image} alt={product.name} />
+	</div>
+	<div class="col-span-3">
+		<p class="text-white font-bold text-xl">{product.name}</p>
+		<p class="text-neutral-500">{product.description}</p>
+	</div>
+	<div class="col-span-2">
+		<p class="text-white">${parseFloat(product.price).toFixed(2)} USD</p>
+	</div>
+	<button
+		class="absolute top-0 right-0 rounded-full p-1 border border-white opacity-0 group-hover:opacity-100 hover:border-red-500 duration-300"
+	>
+		<img src="/delete-icon.png" alt="" class="h-4 w-4" />
+	</button>
+</div>

e20d3760d499ebd311ac31cc7a2c9d71f33a4dc6
ZulianTiger <ady0606@gmail.com>
2023-11-06 22:42:42 +0100

    Lesson 10.5: CartButton component improvements

--- src/lib/components/CartButton.svelte
+++ src/lib/components/CartButton.svelte
@@ -1,3 +1,21 @@
-<div class="border border-white rounded-lg p-2 hover:border-blue-500 duration-300">
+<script>
+	import { get } from 'svelte/store';
+	import { cartItems } from '../stores/cart';
+
+	let quantity = get(cartItems).length;
+
+	cartItems.subscribe((newCartItems) => {
+		quantity = newCartItems.length;
+	});
+</script>
+
+<div class="relative border border-white rounded-lg p-2 hover:border-blue-500 duration-300">
 	<img src="shopping-cart.png" alt="Cart" class="h-4" />
+	{#if quantity > 0}
+		<div
+			class="absolute top-[-10px] right-[-10px] p-1 w-5 h-5 bg-blue-500 flex justify-center items-center rounded-full"
+		>
+			<span class="text-xs text-white">{quantity}</span>
+		</div>
+	{/if}
 </div>
